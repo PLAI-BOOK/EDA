765c075575f3e936ae9589839d04f15ffc05741d
hallelhel <hallelh@post.bgu.ac.il>
2024-11-05 21:15:46 +0200

    fixed saving into the dir - pushed by Omri

--- SQLtoDF.py
+++ SQLtoDF.py
@@ -12,9 +12,7 @@ def SQLtoDF(table_name):
 
 def DFtoCSVandSAVE(table_name,file_path):
     df = SQLtoDF(table_name)
-    file_name = table_name + ".csv"
-    full_file_path = os.path.join(file_path, file_name)
-    csv = df.to_csv(str(full_file_path),index=False)
+    csv = df.to_csv(str(file_path),index=False)
 
 def DB_backup_low_version():
     print("This is for the first backup - until Shapira fixes Postgres backup")
@@ -28,13 +26,13 @@ def DB_backup_low_version():
     # Save each table as a CSV in the backup directory
     for table_name in tables_names:
         file_path = os.path.join(backup_dir, f"{table_name}.csv")
-        DFtoCSVandSAVE(table_name)
+        DFtoCSVandSAVE(table_name,file_path)
         print(f"{table_name} backup is done and saved as {file_path}")
 
     print(f"All backups are saved in the '{backup_dir}' directory.")
 
 
-
+DB_backup_low_version()
 
 
 
