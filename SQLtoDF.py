65f3d3d10e9192fd3a9d8cf936a230baf88e3d6b
Omri Peretz <127842154+OmriPere@users.noreply.github.com>
2024-11-05 20:55:37 +0200

    added function for low solution backup for DB

--- SQLtoDF.py
+++ SQLtoDF.py
@@ -7,3 +7,20 @@ def SQLtoDF(table_name):
     # Query the table and load it into a DataFrame
     df = pd.read_sql_table(table_name, con=engine)
     return df
+
+def DFtoCSVandSAVE(table_name):
+    df = SQLtoDF(table_name)
+    file_name = table_name + ".csv"
+    csv = df.to_csv(file_name,index=False)
+
+def DB_backup_low_version():
+    print("this is for first backup - until Shapira will fix postgres backup")
+    tables_names = ['events', 'fixtures', 'players', 'teams', 'game_possession', 'whoscored_events']
+    for table_name in tables_names:
+        DFtoCSVandSAVE(table_name)
+        print(f"{table_name} is done")
+
+
+
+
+
