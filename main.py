0516865101d12b2c1fa51724005931dc17ab33bc
hallelhel <hallelh@post.bgu.ac.il>
2024-12-07 12:04:58 +0200

    update to use from db_connect.connectDB import get_db_connection as get_db_connection_out

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from db_connect.connectDB import get_db_connection
+import os
+from dotenv import find_dotenv, load_dotenv
+load_dotenv()
+from db_connect.connectDB import get_db_connection as get_db_connection_out
 
-con=get_db_connection()
+
+
+con=get_db_connection_out(db_name="second_league")
+print(con)
+con.close()
+con=get_db_connection_out(db_name="workingdb")
 print(con)
 con.close()
